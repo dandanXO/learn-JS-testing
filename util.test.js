e6a84f0fff2e0a2ead07853b11ac1a20a27aab11
dandanXO <pigpig850130@gmail.com>
2019-03-13 21:07:18 +0800

    add three different test code

--- util.test.js
+++ util.test.js
@@ -1,6 +1,36 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
+const puppeteer = require('puppeteer');
 
+//unit tests
 test('should output name and age', () => {
-    const text = generateText('DAN',23);
-    expect(text).toBe('DAN (23 years old)');
+  const text = generateText('Max', 29);
+  expect(text).toBe('Max (29 years old)');
 });
+
+//integration tests
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Max', 29);
+  expect(text).toBe('Max (29 years old)');
+});
+
+
+//e2e tests
+
+test('should create an element with text and correct class', async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    // slowMo: 80,
+    // args: ['--window-size=1920,1080']
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    'file:///G:/javascriptTesting/js-testing-introduction/index.html'
+  );
+  await page.click('input#name');
+  await page.type('input#name', 'Anna');
+  await page.click('input#age');
+  await page.type('input#age', '28');
+  await page.click('#btnAddUser');
+  const finalText = await page.$eval('.user-item', el => el.textContent);
+  expect(finalText).toBe('Anna (28 years old)');
+}, 10000);
